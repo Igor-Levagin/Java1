7d40807385659c2664853e37bddac12287dbb4be
Igor L <u11.3602z@gmail.com>
2023-07-19 18:57:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int money = 8654;
+        int miles = money / 20;
+
+        System.out.println("При покупке билета, полученно: " + miles + " милей.");
+    }
+}
